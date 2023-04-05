e7241d2b17557896ba9407e50e5c0d6075626ecf
Jessie Guzman <jessieguzman777@gmail.com>
2023-04-05 13:13:18 -0500

    shower/made slight changes to input fields

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "../comp-css/NewCardForm.css"
 
+function handleValueChange(e){
+  console.log(e.target.value)
+}
+
 function NewCardForm(){
   return(
     <div>
       <div className="cardForm">
         <p> Please enter a new card name</p>
-          <input className="formStyle" type="text" placeholder="Enter card name"></input>
+          <input className="formStyle" type="text" onChange={(e) =>handleValueChange(e)} placeholder="Enter card name"></input>
         <p> Please enter the exact set the card is from</p>
          <input className="formStyle" type="text" placeholder="Enter card set"></input>
         <p> Please enter the exact image of the new card from scryfall</p>
@@ -14,8 +18,9 @@ function NewCardForm(){
         <p className="formLegend">Legend: Clicking the checkbox constitutes the card is up for trade.</p>
         <input type="checkbox"></input>
           <label>Is this card Available for trade?</label>
-        <p></p>
-        <button>Submit new card!</button>
+        <p>🃏
+         <button>Submit new card!</button>
+         🃏</p>
         </div>
       </div>
   )

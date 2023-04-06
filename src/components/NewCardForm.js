7d73a167d510cdf76fcd24270910cd7e2593e663
Jessie Guzman <jessieguzman777@gmail.com>
2023-04-05 22:09:42 -0500

    added form handlers and state

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "../comp-css/NewCardForm.css"
 
-function handleValueChange(e){
-  console.log(e.target.value)
-}
+// function handleValueChange(e){
+//   console.log(e.target.value)
+// }
 
 function NewCardForm(){
+  let [cardName, setCardName] = useState('')
+  let [cardSet, setCardSet] = useState('')
+  let [cardImage, setCardImage] = useState('')
+  let [forTrade, setForTrade] = useState(false)
+
+  function handleForm(e){
+    e.preventDefault()
+    const newCardInfo = {
+      cardName,
+      cardSet,
+      cardImage,
+      forTrade
+    }
+    console.log(newCardInfo)
+  }
+
   return(
     <div className="bottom">
       <div className="cardForm">
         <p> Please enter a new card name</p>
-          <input className="formStyle" type="text" onChange={(e) =>handleValueChange(e)} placeholder="Enter card name"></input>
+        <form onSubmit={handleForm}>
+          <input 
+            className="formStyle" 
+            type="text" 
+            value={cardName} 
+            onChange={(e)=>setCardName(e.target.value)}
+            placeholder="Enter card name">
+           </input>
         <p> Please enter the exact set the card is from</p>
-         <input className="formStyle" type="text" placeholder="Enter card set"></input>
+          <input 
+            className="formStyle" 
+            type="text" 
+            value={cardSet} 
+            onChange={(e)=>setCardSet(e.target.value)} 
+            placeholder="Enter card set">
+          </input>
         <p> Please enter the exact image of the new card from scryfall</p>
-        <input className="formStyle" type="text" placeholder="Enter scryfall image URL"></input>
+          <input 
+            className="formStyle" 
+            type="text"
+            value={cardImage}
+            onChange={(e)=>setCardImage(e.target.value)}
+            placeholder="Enter scryfall image URL"> 
+          </input>
         <p className="formLegend">Legend: Clicking the checkbox constitutes the card is up for trade.</p>
-        <input type="checkbox"></input>
+          <input 
+            type="checkbox"
+            checked={forTrade}
+            onChange={(e)=>setForTrade(e.target.checked)}>
+          </input>
           <label>Is this card Available for trade?</label>
         <p>🃏
-         <button>Submit new card!</button>
+         <input type="submit" value={'Submit new card!'}/>
          🃏</p>
+        </form>
         </div>
       </div>
   )
